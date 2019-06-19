18ce19f9a097e29f563cd249a98d4d8aece6e031
HassanMagdyAbdallah <hassanbatboy1@gmail.com>
2019-06-19 22:05:14 +0200

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 06:21:18 2019
+
+@author: Hassan
+"""
+import time
+import urllib
+
+opener = urllib.request.build_opener()
+
+def getNextURL(URL): #Finds the first link/title (broken when it has a country)
+    infile = opener.open(URL)
+    page = infile.read()
+                            #Find the first <p> tag for the main body
+    mainP = page[page.find('<p>'.encode(encoding='utf-8',\
+                errors='strict')):page.find('<p>'.encode(encoding='utf-8',\
+                errors='strict'))+500] 
+                            #Find the first href for the link
+    newPage = mainP[mainP.find('<a href="/wiki/'.encode(encoding='utf-8',\
+                    errors='strict'))+15:mainP.find('"'.encode(encoding='utf-8',\
+                    errors='strict'),mainP.find('<a href="/wiki/'.encode(encoding='utf-8',\
+                    errors='strict'))+15)] 
+    return newPage
+visited = []
+def main():    # The MAIN function
+    URL = 'https://en.wikipedia.org/wiki/Special:Random'
+    counter = 0
+     #for loop detection
+    start_time = time.time()
+    newPage = getNextURL(URL)
+    visited.append(newPage)
+    if newPage.decode(encoding='utf-8', errors='strict') == 'Philosophy':
+        counter += 1
+        print(newPage.decode(encoding='utf-8', errors='strict'))
+    while newPage.decode(encoding='utf-8', errors='strict') != 'Philosophy':
+        #print(URL)
+                        #Creates the next link to go to based upon the first link
+        URL = 'http://en.wikipedia.org/w/index.php?title=' + newPage.decode(encoding='utf-8',\
+                                                                             errors='strict') 
+        time.sleep(.5)
+        newPage = getNextURL(URL)
+        if newPage in visited:
+            print("Loop Detected")
+            break
+        visited.append(newPage)
+        print (newPage.decode(encoding='utf-8', errors='strict'))
+        counter +=1
+    t = time.time()-start_time
+    print(counter) 
+    print(t)
+
+if __name__== "__main__":
+    main()
